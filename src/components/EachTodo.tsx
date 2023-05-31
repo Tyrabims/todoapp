a6b2ce13fa9faa8827e0ee9fcb37da057be35dd3
Tyrabims <abimbola.anibaba@yahoo.com>
2023-05-31 18:30:08 +0100

    almost done

--- src/components/EachTodo.tsx
+++ src/components/EachTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from './Todo';
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md';
@@ -12,27 +12,64 @@ interface Props{
 }
 
 const EachTodo = ({todo, todos, setTodos}: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  
+  useEffect(() => {
+      inputRef.current?.focus()
+  }, [edit])
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
   const handleCompleted = (id:number) => {
     setTodos(todos.map((todo) => (todo.id === id ? {...todo, completeTodo:!todo.completeTodo} : todo)
       )
     )
   };
  
+  const handleDeleted = (id:number) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+  };
+
+  const handleEdit = (event:React.SyntheticEvent, id: number) => {
+      event.preventDefault();
+      setTodos(todos.map((todo) => (
+        todo.id === id ? {...todo,todo : editTodo} : todo ))
+      );
+       setEdit(false);
+  }
+ 
   return (
     <>
-      <form className='each_todo'> 
-         <div style={{color:'white', backgroundColor: 'blue', marginLeft:15, marginRight: 15, padding:15, paddingLeft: 15, borderBottom:20}}>
-         <span className="each_todo_" style={{paddingRight: 710}}>{todo.todo}</span>
-            <span className='icon'>
+      <form className='each_todo' onSubmit={(event) => handleEdit(event, todo.id)}> 
+      <div style={{color:'white', backgroundColor: 'blue', marginLeft: 25, marginRight: 480, marginBottom: 10, padding: 15, paddingLeft: 15, borderBottom: 20}}>
+         {
+          edit ? (
+              <input value={editTodo} className="each_todo_" onChange={(event) => setEditTodo(event.target.value)}/>
+          ): (
+            todo.completeTodo ? (
+              <s className="each_todo_" style={{paddingRight: 900}}>{todo.todo}</s>
+               ) : (
+              <span className="each_todo_" style={{paddingRight: 900}}>{todo.todo}</span> 
+              )
+                  )
+         }
+         
+         
+            <span className='icon' onClick = {() => {
+              if(!edit && !todo.completeTodo) {
+                setEdit(!edit);
+              }
+            }}
+            >
               <AiOutlineEdit />
             </span>
             <span className='icon' onClick={() => handleCompleted(todo.id)}> 
               <MdDone/>
             </span>
-            <span className='icon'>
+            <span className='icon' onClick={() => handleDeleted(todo.id)}>
               <AiOutlineDelete />
             </span>
-            
             </div> 
     </form>
     </>
